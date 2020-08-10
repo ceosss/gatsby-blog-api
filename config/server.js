a0912a6044bf55bbc8ca48197c54120f62336c56
ceosss <sswarajsamant@gmail.com>
2020-08-10 23:57:31 +0530

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1417ff0691bbad55b2ed050010510135'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c2b155c4-b28e-4be8-a8a1-341355fc515d'
+};
